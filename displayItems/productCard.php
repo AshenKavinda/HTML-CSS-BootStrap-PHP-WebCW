1767de2f0902fbebb91bb0fa34c443a4ecde2560
AdeeshaNanayakkara <adeeshananayakkara27@gmail.com>
2024-07-11 03:47:51 +0530

    cardStyles

--- displayItems/productCard.php
+++ displayItems/productCard.php
@@ -15,35 +15,26 @@
       $name = $row[1] ;
       $price = $row[2] ;
       $stock = $row[3] ;
-      $av = "";
+      $av = $stock == 1 ? "Available" : "Out of Stock"; 
 
-      if($stock > 0)
-      {
-        $av = "Available";
-      }else{
-        $av = "Out of Stock";
-      }
-
-      echo "<br>
+      echo "
             
-      <div class='col-3'>
+      <div class='col-md-3'>
           <div class='m-2'>
-            <div class='card shadow'>
+            <div class='card shadow card-bg '>
               <img class='card-img-top' src=../adminPanel/pImg/$image >
 
-              <div class='card-body'>
+              <div class='card-body my-2'>
                 <h5 class='card-title'>$name</h5>
-                <span class='badge badge-success'>Rs.$price.00</span>";
+                <span class='badge badge-success'>Rs.$price.00</span>
 
-                if($av == "Available")
-                {
-                    echo "<span class='badge badge-success' style = 'background-color:green; margin-left: 20px;'>$av</span>";
-                }else if($av == "Out of Stock")
-                {
-                    echo "<span class='badge badge-success' style = 'background-color:red;margin-left: 20px;'>$av</span>";
-                }
-
-                echo "<br><br>
+                <span class='badge ".($av == 'Available' ? 'badge-available' : 'badge-outofstock')."' style='margin-left: 20px;'>$av</span>
+              
+                <div class='w-100 d-flex my-5' >
+                  <div style='badge'>
+                    Quantity <input type='number' id='quantity' name = '' value= '1' style='width:35px; border:none;'>
+                  </div>
+                </div>
                 <a href='#' class='btn btn-primary'>Add to Cart</a>
               </div>
             </div>
@@ -102,14 +93,14 @@
     <br>
   
     <div class="container">
-      
-      <div class="row">
-
-        <?php
-            printAll($result);
-        ?>
- 
+    <div class="mt-3">
+    <div class="row">
+      <?php
+          printAll($result);
+      ?>
       </div>
+    </div>      
+      
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
