e6009b82b9b7e9998faefc42b3030a314db591ba
AdeeshaNanayakkara <adeeshananayakkara27@gmail.com>
2024-07-13 15:42:29 +0530

    final

--- cart/deliveryInformation.php
+++ cart/deliveryInformation.php
@@ -11,7 +11,7 @@ session_start();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
   </head>
-  <body>
+  <body style="background-image: url(../adminPanel/blurpro.jpg); background-size:cover;">
     <div class="container">
         <div class="mt-5 d-flex flex-row gap-4 m-3">
             <a href="cart.php" style="text-decoration: none;color:aliceblue;"><button class="btn btn-primary px-4 mt-2"><</button></a>        
